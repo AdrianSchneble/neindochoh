b323bbe6131f117e89a0f5580a3422442d5c936f
PixelMaster98 <misteradi126@gmail.com>
2017-11-30 00:07:46 +0100

    Bugfixes

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '463156047:AAEwqrz_Ao9SAqlLfJv2HMcNyRioz5f9J8Y';
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, {
+  polling: true
+});
 
 // Matches "/echo [whatever]"
-bot.onText(/^Nein|^Doch/ig, (msg, match) => {
+bot.onText(/^Nein|^Nain|^Nayn|^Neyn|^9|^Doch/ig, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content of the message
-  
+
   const chatId = msg.chat.id;
-  var resp = "";
+  var resp = "empty";
 
   switch (String(match).toLowerCase()) {
     case "nein":
       resp = "Doch!";
       break;
-    case "nein":
+    case "doch":
       resp = "Oh!";
       break;
   }
@@ -26,10 +28,15 @@ bot.onText(/^Nein|^Doch/ig, (msg, match) => {
   bot.sendMessage(chatId, resp);
 });
 
-bot.onText(/[^,] +dass|[^ ]dass/ig), (msg, match) => {
+bot.onText(/[^,] +dass|[^ ]dass/ig, (msg, match) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, "*, dass");
-}
+});
+
+bot.onText(/^penis$/ig, (msg, match) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Adrian approves 😏");
+});
 
 // // Listen for any kind of message. There are different kinds of
 // // messages.
